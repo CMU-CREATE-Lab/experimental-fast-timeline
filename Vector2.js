f561199e54c821a4e200f219d6aff94f696e996b
Gabriel ODonnell <gabriel.odonnell@gmail.com>
2015-05-20 16:00:37 -0400

    Add function for calculating distance between two vectors   * Used for calculating the length of time ticks

--- Vector2.js
+++ Vector2.js
@@ -17,3 +17,11 @@ cr.Vector2.prototype.add = function(v) {
 cr.Vector2.prototype.subtract = function(v){
     return new cr.Vector2(this._x - v._x, this._y - v._y);
 }
+
+cr.Vector2.prototype.distanceSquared = function(v) {
+    return Math.pow(this._x - v._x, 2) + Math.pow(this._y - v._y, 2);
+}
+
+cr.Vector2.prototype.distance = function(v) {
+    return Math.sqrt(this.distanceSquared(v));
+}
