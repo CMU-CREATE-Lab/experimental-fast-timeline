8d0790d83c879f44f08b83ab9e4690f9a41ce33a
Gabriel ODonnell <gabriel.odonnell@gmail.com>
2015-06-02 15:43:22 -0400

    Manage tiles for rendering in webgl or 2d canvas contexts

--- DataStoreTileLayer.js
+++ DataStoreTileLayer.js
@@ -1,19 +1,24 @@
 "use strict";
 
-function DataStoreTileLayer(glb, rootUrl) {
+function DataStoreTileLayer(rootUrl, glb, ctx) {
   this.glb = glb;
-  this.gl = glb.gl;
+  this.ctx = ctx;
   this._rootUrl = rootUrl;
+  var that = this;
 
   function createTile(ti, bounds) {
     var url = rootUrl + '/' + ti.l + '.' + ti.o;
-    return new DataStoreTile(glb, ti, url);
+    if (that.glb && that.useWebgl) {
+      return new DataStoreTile(glb, ti, url);
+  } else {
+      return new CanvasTile(ctx, ti, url);
+    }
   }
 
   this._tileView = new TileView({
       createTile: createTile,
       deleteTile: function(tile) {},
-      updateTile: DataStoreTile.update
+      updateTile: (that.glb && that.useWebgl) ? DataStoreTile.update : CanvasTile.update
     });
 
   this.destroy = function() {
@@ -23,6 +28,16 @@ function DataStoreTileLayer(glb, rootUrl) {
 }
 
 DataStoreTileLayer.prototype.draw = function(view) {
+    if (this.glb && this.useWebgl) {
+        this.drawWebgl(view);
+    } else {
+        this.drawCanvas(view);
+    }
+}
+
+DataStoreTileLayer.prototype.drawWebgl = function(view) {
+    this.glb.gl.clear(plot.gl.COLOR_BUFFER_BIT);
+
     var pMatrix = new Float32Array([1, 0, 0, 0,
                                 0, 1, 0, 0,
                                 0, 0, 1, 0,
@@ -39,4 +54,20 @@ DataStoreTileLayer.prototype.draw = function(view) {
 
     this._tileView.setView({min:view.xmin, max:view.xmax});
     this._tileView.update(pMatrix);
+}
+
+
+DataStoreTileLayer.prototype.drawCanvas = function(view) {
+    this.ctx.clearRect (0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+
+    var transform = {};
+    transform.xOffset = -view.xmin;
+    transform.xScale = plot.canvas2d.width / (view.xmax - view.xmin);
+    transform.yOffset = -view.ymax;
+    transform.yScale = plot.canvas2d.height / (view.ymin - view.ymax);
+
+    this._tileView.setView({min:view.xmin, max:view.xmax});
+    this._tileView.update(transform);
+
+
 }
