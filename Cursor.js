1f8a61f69979bf48fd9e568a718fce82784ce88b
Gabriel ODonnell <gabriel.odonnell@gmail.com>
2015-08-05 15:01:30 -0400

    Update Highlight and Cursor

--- Cursor.js
+++ Cursor.js
@@ -9,13 +9,55 @@ cr.Cursor = function (plotDiv) {
     this._cursor.style["borderWidth"] = "20px 10px 0 10px";
     this._cursor.style["borderColor"] = "rgb(255,0,0) transparent transparent transparent";
     this._cursor.style["position"] = "absolute";
-    this._cursor.style["display"] = "none";
+    this._cursor.style["marginTop"] = Math.ceil(plotDiv.offsetHeight / 2) + "px";
+    this._cursor.style["display"] == "none";
+    //this._cursor.style["display"] = "none";
     plotDiv.appendChild(this._cursor);
+
+    this.x = null;
+
+    this.timeGraphAxis = null;
+
+    $('#cursor').mousedown(this,this.mousedown);
+    $('#cursor').mousemove(this,this.mousemove);
+    $('#cursor').mouseup(this, this.mouseup);
+
+    this.lastMouse = null;
 }
 
-cr.Cursor.prototype.draw = function(transform) {
-    if (this._cursor.style["display"] == "none") {
+cr.Cursor.prototype.update = function(x) {
+    if (this.x) {
         this._cursor.style["display"] = "block";
+        this._cursor.style["left"] = x + "px";
     }
-    this._cursor.style["left"] = (transform.xScale * (this.position + transform.xOffset))/window.devicePixelRatio - 10 + "px";
+}
+cr.Cursor.prototype.mousedown = function(e) {
+  var that = e.data;
+  that.lastMouse = e;
+  return false;
+
+}
+
+
+cr.Cursor.prototype.mousemove = function(e) {
+    var that = e.data;
+    if (!e.which) {
+        that.mouseup(e);
+        return;
+    }
+
+    if (that.lastMouse) {
+        var dx = e.clientX - that.lastMouse.clientX;
+        var currentPx = parseInt(that._cursor.style["left"]);
+        currentPx += dx;
+        that._cursor.style["left"] = currentPx + "px";
+        //that.update(dx);
+        that.lastMouse = e;
+    }
+    return false;
+}
+
+cr.Cursor.prototype.mouseup = function(e) {
+    var that = e.data;
+    that.lastMouse = null;
 }
--- Highlight.js
+++ Highlight.js
@@ -1,47 +1,72 @@
 var cr = cr || {}
 
 cr.Highlight = function (plotDiv) {
-    this._highlight = document.createElement("canvas");
-    this._highlight.setAttribute("id", "highlight");
-    this._highlight.style["width"] = "100%";
-    this._highlight.style["height"] = "100%";
-    this._highlight.style["position"] = "absolute";
-    this._highlight.style.pointerEvents = 'none';
+    this.div = plotDiv;
+    this.canvas = document.createElement("canvas");
+    this.canvas.setAttribute("id", "highlight");
+    this.canvas.style["width"] = plotDiv.style["width"];
+    this.canvas.style["height"] = plotDiv.style["height"];
+    this.canvas.style["position"] = "absolute";
+    this.canvas.style.pointerEvents = 'none';
+    this.resolutionScale = window.devicePixelRatio || 1;
+    this.canvas.height = this.div.clientHeight * this.resolutionScale;
+    this.canvas.width = this.div.clientWidth * this.resolutionScale;
 
-    plotDiv.appendChild(this._highlight);
-    this._ctx = this._highlight.getContext('2d');
+    plotDiv.appendChild(this.canvas);
+    this.ctx = this.canvas.getContext('2d');
+    this.ctx.scale(this.resolutionScale,this.resolutionScale);
+
+    this.points = {};
 }
 
-cr.Highlight.prototype.drawPoint = function(transform, view) {
-    this._ctx.beginPath();
-    this._ctx.arc(transform.xScale * (this.point.x + transform.xOffset),
-        transform.yScale * (this.point.y + transform.yOffset),
-        3*window.devicePixelRatio,
-        0,
-        Math.PI*2,
-        true);
-    this._ctx.fillStyle = "rgb(255,0,0)";
-    this._ctx.fill();
+cr.Highlight.prototype.drawPoints = function(points) {
+    if (this.points) {
+        for (var i = 0; i < points.length; i++) {
+          this.ctx.beginPath();
+          this.ctx.arc(points[i].x, this.canvas.height - points[i].y, 3*window.devicePixelRatio, 0, Math.PI*2, true);
+          this.ctx.fillStyle = "rgb(255,0,0)";
+          this.ctx.fill();
+      }
+  }
 }
 
 cr.Highlight.prototype.drawMOPoint = function(transform, view) {
-    this._ctx.beginPath();
-    this._ctx.arc(transform.xScale * (this.mopoint.x + transform.xOffset),
+    if (this.point) {
+      this.ctx.beginPath();
+      this.ctx.arc(transform.xScale * (this.mopoint.x + transform.xOffset),
         transform.yScale * (this.mopoint.y + transform.yOffset),
         3*window.devicePixelRatio,
         0,
         Math.PI*2,
         true);
-    this._ctx.fillStyle = "rgb(255,0,0)";
-    this._ctx.fill();
+    this.ctx.fillStyle = "rgb(255,0,0)";
+    this.ctx.fill();
+  }
+}
+cr.Highlight.prototype.drawLine = function(x) {
+    if (this.anchor) {
+        this.ctx.clearRect (0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.beginPath();
+        this.ctx.lineWidth = 1.*window.devicePixelRatio;
+        this.ctx.strokeStyle = "rgb(255,0,0)";
+        this.ctx.moveTo(x,0);
+        this.ctx.lineTo(x,this.canvas.height);
+        this.ctx.stroke();
+    }
 }
 
-cr.Highlight.prototype.drawLine = function(transform, view) {
-    this._ctx.beginPath();
-    this._ctx.lineWidth = 1.5*window.devicePixelRatio;
-    this._ctx.strokeStyle = "rgb(255,0,0)";
-    this._ctx.moveTo(transform.xScale * (this.line.x + transform.xOffset),0);
-    this._ctx.lineTo(transform.xScale * (this.line.x + transform.xOffset),
-        transform.yScale * (view.ymin + transform.yOffset));
-    this._ctx.stroke();
+cr.Highlight.prototype.resize = function() {
+    var canvasWidth = this.div.clientWidth * window.devicePixelRatio;
+    var canvasHeight = this.div.clientHeight * window.devicePixelRatio;
+    if (this.canvas.width != canvasWidth ||
+        this.canvas.height != canvasHeight) {
+      this.canvas.width = canvasWidth;
+      this.canvas.height = canvasHeight;
+
+      this.canvas.style["width"] = this.div.style["width"];
+      this.canvas.style["height"] = this.div.style["height"];
+
+    }
+
+
 }
