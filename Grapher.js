62c55f0468d21092f417e194bf5ee0ae8a16b004
Chris Bartley <bartley@cmu.edu>
2015-10-15 10:14:32 -0400

    PlotContainer and SeriesPlotContainer now require the date axis to be passed in
    This change helps by 1) eliminating the need for the getXAxis() method, which tried to figure out the date axis by looking at the contained plots. But, if there are no contained plots, getXAxis() returns null, even though there actually is a date axis associated with every plot container; and 2) no more chance of NPE's in methods which used to call getXAxis() since the X axis can no longer be null.

--- Grapher.js
+++ Grapher.js
@@ -237,11 +237,12 @@ var DataSeriesPlot = function(datasource, horizontalAxis, verticalAxis, options)
  * @param {string} elementId - the DOM element ID for the container div holding this plot container
  * @param {boolean} ignoreClickEvents - whether to ignore click events, currently ignored
  * @param {cr.Plot[]} plots - array of plots to be added to the plot container
+ * @param {cr.TimeGraphAxis} dateAxis - the date axis
  * @param {object} [options] - additional optional options
  * @return {cr.SeriesPlotContainer}
  */
-var PlotContainer = function(elementId, ignoreClickEvents, plots, options) {
-    return new cr.SeriesPlotContainer(elementId, plots, options)
+var PlotContainer = function(elementId, ignoreClickEvents, plots, dateAxis, options) {
+    return new cr.SeriesPlotContainer(elementId, plots, dateAxis, options)
 };
 
 var SequenceNumber = function() {
